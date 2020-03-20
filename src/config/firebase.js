282ee736a5b527b50b4a74b1f7328fa8eff996e5
aaronleechan <aaron171091@gmail.com>
2020-03-20 16:11:40 -0700

    update firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -8,8 +8,6 @@ const config = {
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const firebaes = firebase.initializeApp(config);
